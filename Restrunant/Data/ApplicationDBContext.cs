9ecac46efcf41b71bd7c0836d28d570aaa22a6c4
Shag0r <shagorkumardas@gmail.com>
2024-08-21 18:17:16 +0600

    Add project files.

---
+++ Restrunant/Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restrunant.Model;
+
+namespace Restrunant.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) 
+        {
+            
+        }
+        public DbSet<Category> Category{ get; set; }
+    }
+}
