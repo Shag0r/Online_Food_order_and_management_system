adb16b92062e248e8c1c7be126683fe668ca9aa9
Shag0r <shagor.kumar@eps.com.bd>
2024-10-13 16:19:35 +0600

    Repository added

---
+++ Restrunant.DataAccess/Repository/Repository.cs
@@ -0,0 +1,44 @@
+﻿using Restrunant.DataAccess.Data;
+using Restrunant.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restrunant.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDBContext _db;
+        public Repository(ApplicationDBContext db)
+        {
+            _db = db; 
+        }
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>>? filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
